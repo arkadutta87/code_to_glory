1d0451259a36080b4cb1995f5c95ad52f705b837
Arka Dutta <arkadutta@Arkas-MacBook-Air.local>
2026-01-28 21:49:34 +0530

    House Robber leetcode problem - solution

---
+++ src/main/java/org/leetcode/dynamic_programming/HouseRobber.java
@@ -0,0 +1,46 @@
+package org.leetcode.dynamic_programming;
+
+import java.util.HashMap;
+
+public class HouseRobber {
+
+    public int rob(int[] nums) {
+        HashMap<Integer, Integer> memoization = new HashMap<>();
+        return maximiseRobbery(nums, -1, memoization);
+    }
+
+    private int maximiseRobbery(int[] nums, int currentHouse, HashMap<Integer, Integer> memoization) {
+
+        if (currentHouse >= nums.length) {
+            return 0;
+        }
+
+        if (memoization.containsKey(currentHouse)) {
+            return memoization.get(currentHouse);
+        }
+
+        int houseIndex1, houseIndex2 = 0;
+
+        if (currentHouse == -1) {
+            houseIndex1 = 0;
+            houseIndex2 = 1;
+        } else {
+            houseIndex1 = currentHouse + 2;
+            houseIndex2 = currentHouse + 3;
+        }
+
+        memoization.put(houseIndex1, maximiseRobbery(nums, houseIndex1, memoization));
+        memoization.put(houseIndex2, maximiseRobbery(nums, houseIndex2, memoization));
+
+        memoization.put(currentHouse, (currentHouse == -1 ? 0 : nums[currentHouse]) +
+                Math.max(memoization.get(houseIndex1), memoization.get(houseIndex2)));
+
+        return memoization.get(currentHouse);
+    }
+
+    public static void main(String[] args) {
+        HouseRobber robber = new HouseRobber();
+        System.out.println(robber.rob(new int[]{1, 2, 3, 1}));
+        System.out.println(robber.rob(new int[]{2, 7, 9, 3, 1}));
+    }
+}
---
+++ src/test/java/org/leetcode/dynamic_programming/HouseRobberTest.java
@@ -0,0 +1,16 @@
+package org.leetcode.dynamic_programming;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.leetcode.design.LoggerRateLimiter;
+
+public class HouseRobberTest {
+
+    @Test
+    public void testPositiveCase() {
+        HouseRobber robber = new HouseRobber();
+
+        Assertions.assertEquals(robber.rob(new int[]{1, 2, 3, 1}),  4);
+        Assertions.assertEquals(robber.rob(new int[]{2, 7, 9, 3, 1}), 12);
+    }
+}
